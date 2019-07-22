6d9b2fc0b6f017c2a78f2044d2b3836c07929dfa
MingqianYang <Mingqian.Yang-1@student.uts.edu.au>
2019-07-22 13:53:54 +1000

    every frame increments an int varriable by 1

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -6,34 +6,17 @@ public class HelloWorld : MonoBehaviour
 {
 
     private int number = 0;
-    float _t = 0f;
+
 
     // Update is called once per frame
     void Update()
     {
-        _t += Time.deltaTime;
-
-        if (_t >= 1f)
-        {
-            _t = 0f;
-            Debug.Log("the number is : " + number++.ToString());
-        }
+        Debug.Log("the number is : " + number++.ToString());
     }
 
     void Start()
     {
-        //StartCoroutine(WaitAndPrint());
-    }
-
-    IEnumerator WaitAndPrint()
-    {
-        while (true)
-        {
-            Debug.Log("the number is : " + number++.ToString());
-
-            yield return new WaitForSeconds(1f);
-        }
-
+       
     }
 
 }
