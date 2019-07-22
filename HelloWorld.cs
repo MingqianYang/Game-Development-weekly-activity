b3ddb3113b2760f798aad079e217ea09c8ddc151
MingqianYang <Mingqian.Yang-1@student.uts.edu.au>
2019-07-22 13:29:08 +1000

    Create a new C# script called HelloWorld that every frame increments an int variable by 1

---
+++ HelloWorld.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+
+    private int number = 0;
+    float _t = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _t += Time.deltaTime;
+
+        if (_t >= 1f)
+        {
+            _t = 0f;
+            Debug.Log("the number is : " + number++.ToString());
+        }
+    }
+
+    void Start()
+    {
+        //StartCoroutine(WaitAndPrint());
+    }
+
+    IEnumerator WaitAndPrint()
+    {
+        while (true)
+        {
+            Debug.Log("the number is : " + number++.ToString());
+
+            yield return new WaitForSeconds(1f);
+        }
+
+    }
+
+}
