8f56d9d9f273f8408b52af8d4e219b48a12a711e
MingqianYang <Mingqian.Yang-1@student.uts.edu.au>
2019-08-12 17:00:24 +1000

    add week4

---
+++ Lab Week 4 - Activity/Assets/Scripts/PrintAndHide.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Random = System.Random;
+
+public class PrintAndHide : MonoBehaviour
+{
+    private int i;
+    private int randomNumber;
+    public Renderer rend;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 0;
+        Random random = new Random();
+        randomNumber =  random.Next(200, 250);
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log($"{gameObject.name}:  {i++} ");
+
+        if (GameObject.FindWithTag("Red") && i ==100)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (GameObject.FindWithTag("Blue") && i == randomNumber)
+        {
+            rend.enabled = false;
+        }
+    }
+}
