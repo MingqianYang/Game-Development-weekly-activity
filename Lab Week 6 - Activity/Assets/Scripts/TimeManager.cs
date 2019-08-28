6d49bde358916e1c8ac746ce8dd43a182bfc1d24
MingqianYang <Mingqian.Yang-1@student.uts.edu.au>
2019-08-28 12:52:58 +1000

    finish 100%

--- Lab Week 6 - Activity/Assets/Scripts/TimeManager.cs
+++ Lab Week 6 - Activity/Assets/Scripts/TimeManager.cs
@@ -1,16 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
+    [SerializeField] private Transform[] transformArray;
+    const float moveWait = 2.0f;
+
+    const float scaleWait = 4.0f;
+
     private int lastTime = 0;
     private float timer = 0.0f;
     private int counter = 0;
+    private Vector3[] positions = { new Vector3(2,1,0), new Vector3(2, -1,0) , new Vector3(-2, -1,0) , new Vector3(-2, 1,0) };
 
     // Start is called before the first frame update
     void Start()
     {
+        Camera.main.orthographic = true;
+        Camera.main.orthographicSize = 2.0f;
         ResetTime();
     }
 
@@ -41,14 +50,23 @@ public class TimeManager : MonoBehaviour
             }
             
             Debug.Log("Spacebar pressed");
+
+
+            CancelInvoke("ScaleObjects");
         }
 
-        // 60%        timer += Time.deltaTime;
-        if (timer >= 1f)
+        // 60%        
+        timer += Time.deltaTime;
+        if (timer - lastTime >= 1f)
         {
-            timer -= 1.0f;
-            Debug.Log(counter++);
+            lastTime = (int)timer;
+          
+            Debug.Log(lastTime-1);
+
+            MoveObjects();
+
         }
+
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
@@ -56,11 +74,80 @@ public class TimeManager : MonoBehaviour
         }
 
 
+        // 100%
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            float randomValue = UnityEngine.Random.Range(0.25f, 0.75f);
+            StartCoroutine("RotateObjects", randomValue);
+
+        }
+
     }
 
     private void ResetTime()
     {
-        counter = 0;
+        timer = 0;
+        lastTime = 0;
+
+
+        // The second parameter value?
+         InvokeRepeating("ScaleObjects", 0.0f, scaleWait);
+
+        
     }
 
+    private void MoveObjects()
+    {
+        // move square clockwise
+        if ((lastTime - 1) % moveWait == 0 && (lastTime - 1) >= moveWait)
+        {
+
+            for (int i = 0; i < transformArray.Length; i++)
+            {
+                int index = Array.IndexOf(positions, transformArray[i].position);
+                if (index + 1 >= positions.Length)
+                {
+                    index = -1;
+                }
+                transformArray[i].position = positions[index + 1];
+            }
+
+        }
+    }
+
+    // 90%
+    private void ScaleObjects()
+    {
+        for (int i = 0; i < transformArray.Length; i++)
+        {
+            if (transformArray[i].localScale.x > 1.5)
+            {
+                transformArray[i].localScale = transformArray[i].localScale / 1.2F;
+            }
+            else
+            {
+                transformArray[i].localScale = transformArray[i].localScale * 1.2F;
+
+            }            
+
+        }
+    }
+
+    IEnumerator RotateObjects(float randomDelay)
+    {
+        yield return new WaitForSeconds(randomDelay);
+        for (int i = 0; i < transformArray.Length; i++)
+        {
+            transformArray[i].Rotate(Vector3.forward,  90, Space.World);
+        }
+
+        for (int j = 0; j < 3; j++)
+        {
+            yield return new WaitForSeconds(randomDelay);
+            for (int i = 0; i < transformArray.Length; i++)
+            {
+                transformArray[i].Rotate(Vector3.forward, 90, Space.World);
+            }
+        }
+    }
 }
