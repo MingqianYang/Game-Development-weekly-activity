a934c0839c0eece5e5de0d61875f5fc4873d95b5
MingqianYang <Mingqian.Yang-1@student.uts.edu.au>
2019-09-10 19:03:31 +1000

    Cubic Interpolation

---
+++ Lab Week 7 - Activity/Assets/Scripts/InputManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private GameObject item;
+
+    private Tweener tweener;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tweener = GetComponent<Tweener>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Vector3 endPos = new Vector3(-2.0f, 0.5f, 0.0f);
+            float duration = 1.5f;
+
+            tweener.AddTween(item.transform, item.transform.position, endPos, duration);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Vector3 endPos = new Vector3(2.0f, 0.5f, 0.0f);
+            float duration = 1.5f;
+
+            tweener.AddTween(item.transform, item.transform.position, endPos, duration);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 endPos = new Vector3(0.0f, 0.5f, -2.0f);
+            float duration = 0.5f;
+
+            tweener.AddTween(item.transform, item.transform.position, endPos, duration);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Vector3 endPos = new Vector3(0.0f, 0.5f, 2.0f);
+            float duration = 0.5f;
+
+            tweener.AddTween(item.transform, item.transform.position, endPos, duration);
+        }
+
+    }
+}
---
+++ Lab Week 7 - Activity/Assets/Scripts/Tweener.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tweener : MonoBehaviour
+{
+    private Tween activeTween;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distance = Vector3.Distance(activeTween.Target.position, activeTween.EndPos);
+        if (distance > 0.1f)
+        {
+            float timeFraction = (Time.time - activeTween.StartTime) / activeTween.Duration;
+
+            // Linear interpolation
+            //  activeTween.Target.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, timeFraction);
+
+            // Cubic easing-in interplation
+            activeTween.Target.position = Coserp(activeTween.StartPos, activeTween.EndPos, timeFraction);  
+
+        }
+
+        if (distance <= 0.1f)
+        {
+            activeTween.Target.position = activeTween.EndPos;
+            activeTween = null;
+        }
+
+    }
+
+    //Ease in http://wiki.unity3d.com/index.php?title=Mathfx#C.23_-_Mathfx.cs
+    public static float Coserp(float start, float end, float value)
+    {
+        return Mathf.Lerp(start, end, 1.0f - Mathf.Cos(value * Mathf.PI * 0.5f));
+    }
+
+    public static Vector3 Coserp(Vector3 start, Vector3 end, float value)
+    {
+        return new Vector3(Coserp(start.x, end.x, value), Coserp(start.y, end.y, value), Coserp(start.z, end.z, value));
+    }
+
+
+
+    public void AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
+    {
+        if (activeTween == null)
+        {
+            activeTween = new Tween(targetObject, startPos, endPos, Time.time, duration);
+        }
+    }
+}
