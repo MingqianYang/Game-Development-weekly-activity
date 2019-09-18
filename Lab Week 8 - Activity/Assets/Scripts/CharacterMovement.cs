58014702fc37ad3e7a33ecf366c15d08e7f01bfc
MingqianYang <Mingqian.Yang-1@student.uts.edu.au>
2019-09-18 18:46:06 +1000

    finish 100%

--- Lab Week 8 - Activity/Assets/Scripts/CharacterMovement.cs
+++ Lab Week 8 - Activity/Assets/Scripts/CharacterMovement.cs
@@ -8,16 +8,29 @@ public class CharacterMovement : MonoBehaviour
     private float movementSqrMagnitude;
 
     public float walkSpeed = 1.5f;
+    public Animator animator;
+    public AudioSource footstepSource;
+    public AudioClip[] footstepClips;
+    public AudioSource backgroundSource;
 
     // Update is called once per frame
     void Update()
     {
-        GetMovementInput();
-        CharacterPostion();
-        CharacterRotation();
-        WalkingAnimation();
-        FootstepAudio();
 
+        GetMovementInput();
+        if (RaycastCheck())
+        {
+            CharacterRotation();
+        }
+        else
+        {
+           
+            CharacterPostion();
+            CharacterRotation();
+            WalkingAnimation();
+            FootstepAudio();
+        }
+      
     }
 
 
@@ -30,11 +43,11 @@ public class CharacterMovement : MonoBehaviour
         // Store the squared magnitude of the movement vector 
         movementSqrMagnitude = Vector3.SqrMagnitude(movement);
 
-        Debug.Log(movement);
+       // Debug.Log(movement);
     }
     void CharacterPostion()
     {
-        //gameObject.
+ 
         transform.Translate( movement * walkSpeed * Time.deltaTime, Space.World);
        // transform.position += movement * walkSpeed * Time.deltaTime;
         
@@ -49,7 +62,71 @@ public class CharacterMovement : MonoBehaviour
 
 
     }
-    void WalkingAnimation() { }
-    void FootstepAudio() { }
+    void WalkingAnimation()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetFloat("MoveSpeed", movementSqrMagnitude);
+    }
+    void FootstepAudio()
+    {
+        if (movementSqrMagnitude > 0.3f && !footstepSource.isPlaying)
+        {
+            if (footstepSource.clip.Equals(footstepClips[0]))
+            {
+                footstepSource.clip = footstepClips[1];
+            }
+            else if (footstepSource.clip.Equals(footstepClips[1]))
+            {
+                footstepSource.clip = footstepClips[0];
+            }
 
+
+            // Set the volume of the audio source to the movementSqrMagnitude
+            footstepSource.volume = movementSqrMagnitude;
+            footstepSource.Play();
+
+            //“Duck” the background music volume by reducing it to 0.5f.
+            backgroundSource.volume = 0.5f;
+        }
+        else if (movementSqrMagnitude<=0.3f && footstepSource.isPlaying)
+        {
+            footstepSource.Stop();
+            backgroundSource.volume = 1.0f;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Trigger Exit: " + other.gameObject.name + " : " + other.gameObject.transform.position );
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision Enter: " + collision.gameObject.name + " : " + collision.GetContact(0).point);
+     }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Impassable"))
+        {
+            Debug.Log("Collision Stay: " + collision.gameObject.name);
+        }
+
+    }
+
+    private bool RaycastCheck()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + new Vector3(0, 0.5f, 0), transform.TransformDirection(Vector3.forward), out hit))
+        {
+            Debug.Log("Raycast Hit: " + hit.transform.name);
+            if (hit.transform.CompareTag("Freeze"))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
